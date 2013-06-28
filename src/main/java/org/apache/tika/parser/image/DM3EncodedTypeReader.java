f10cf710656322bfbee64c45dfc35eebe7d7521c
V. Glenn Tarcea <gtarcea@umich.edu>
2013-06-28 10:15:44 -0400

    Fill in COMPLEX reader.

--- src/main/java/org/apache/tika/parser/image/DM3EncodedTypeReader.java
+++ src/main/java/org/apache/tika/parser/image/DM3EncodedTypeReader.java
@@ -1,6 +1,11 @@
 package org.apache.tika.parser.image;
 
+import java.io.IOException;
 import java.nio.ByteOrder;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
 
 public enum DM3EncodedTypeReader
 {
@@ -24,7 +29,7 @@ public enum DM3EncodedTypeReader
             DM3TagType tagType = DM3TagType.toTagType(streamReader.readInt());
             if (tagType != DM3TagType.STRING)
             {
-                // do something here...
+                throw new IllegalStateException("Unexpected tag: " + tagType);
             }
 
             int length = streamReader.readInt();
@@ -80,7 +85,131 @@ public enum DM3EncodedTypeReader
         @Override
         public MetaTuple readEntry(String label, EasyStreamReader streamReader, ByteOrder order)
         {
-            return new MetaTuple(null, null);
+            DM3TagType tagType = DM3TagType.toTagType(streamReader.readInt());
+            if (tagType == DM3TagType.STRUCT)
+            {
+                return readStructEntry(label, streamReader, order);
+            }
+            else if (tagType == DM3TagType.ARRAY)
+            {
+                return readArrayEntry(label, streamReader, order);
+            }
+            else
+            {
+                // Do something here
+                return null;
+            }
+        }
+        
+        private MetaTuple readStructEntry(String label, EasyStreamReader streamReader, ByteOrder order)
+        {
+            /*
+             *  Skip first 4 bytes (length of data area. We will know how much to read from the
+             *  number of fields, and the size of each field.
+             */
+            streamReader.skipBytes(4);
+            int numberOfFields = streamReader.readInt();
+            // Skip unused 4 bytes
+            streamReader.skipBytes(4);
+            List<String> values = new ArrayList<>();
+            for (int i = 0; i < numberOfFields; i++)
+            {
+                DM3SimpleTagTypeReader simpleTagTypeReader = DM3SimpleTagTypeReader
+                        .toSimpleTagTypeReader(streamReader.readInt());
+                String value = simpleTagTypeReader.readValue(order, streamReader);
+                values.add(value);
+            }
+            /*
+             * Read last entry, now we need to skip forward some amount to the next tag.
+             */
+            skipToNextTag(streamReader);
+            
+            String metaValue = StringUtils.join(values, ",");
+            return new MetaTuple(label, metaValue);
+        }
+        
+        private void skipToNextTag(EasyStreamReader streamReader)
+        {
+            byte[] bytesToPeek = peek(streamReader, 28);
+            final int[] jumps = { 4, 3, 3, 5, 5, 3 };
+            int amountToSkip = 0;
+            for (int i = 0; i < jumps.length; i++)
+            {
+                amountToSkip += jumps[i] + 1;
+                byte b = bytesToPeek[amountToSkip-1];
+                DM3TagEntryFormat tagEntryFormat = DM3TagEntryFormat.toEntryType(b);
+                //System.out.println("in loop amountToSkip = " + amountToSkip + "/b = " + b);
+                if (tagEntryFormat != DM3TagEntryFormat.UNKNOWN)
+                {
+                    break;
+                }
+            }
+            
+            //System.out.println("Skipping " + (amountToSkip - 1));
+            streamReader.skipBytes(amountToSkip - 1);
+            //System.out.println("checking stream location = " + esr.readByte());
+        }
+        
+        private byte[] peek(EasyStreamReader streamReader, int bytes)
+        {
+            byte[] buffer = new byte[bytes];
+            try
+            {
+                streamReader.getInputStream().peek(buffer);
+            }
+            catch (IOException e)
+            {
+            }
+            
+            return buffer;
+        }
+        
+        private MetaTuple readArrayEntry(String label, EasyStreamReader streamReader, ByteOrder order)
+        {
+            DM3TagType tagType = DM3TagType.toTagType(streamReader.readInt());
+            /*
+             * Should be STRUCT (15)
+             */
+            if (tagType != DM3TagType.STRUCT)
+            {
+                throw new IllegalStateException("Type not exected: " + tagType);
+            }
+            else
+            {
+                streamReader.skipBytes(4);
+                int numberOfFields = streamReader.readInt();
+                List<DM3SimpleTagTypeReader> dataTypeReaders = getDataTypeReaders(streamReader, numberOfFields);
+                String metavalue = getValues(dataTypeReaders, numberOfFields, order, streamReader);
+                return new MetaTuple(label, metavalue);
+            }
+        }
+        
+        private List<DM3SimpleTagTypeReader> getDataTypeReaders(EasyStreamReader streamReader, int numberOfFields)
+        {
+            List<DM3SimpleTagTypeReader> dataTypeReaders = new ArrayList<>(numberOfFields);
+            for (int i = 0; i < numberOfFields; i++)
+            {
+                streamReader.skipBytes(4);
+                DM3SimpleTagTypeReader tagReader = DM3SimpleTagTypeReader.toSimpleTagTypeReader(streamReader.readInt());
+                dataTypeReaders.set(i, tagReader);
+            }
+            return dataTypeReaders;
+        }
+        
+        private String getValues(List<DM3SimpleTagTypeReader> dataTypeReaders, int numberOfFields, ByteOrder order, EasyStreamReader streamReader)
+        {
+            int length = streamReader.readInt();
+            List<String> values = new ArrayList<>();
+            for (int k = 0; k < length; k++)
+            {
+                for (int q = 0; q < numberOfFields; q++)
+                {
+                    DM3SimpleTagTypeReader simpleTagTypeReader = dataTypeReaders.get(q);
+                    String value = simpleTagTypeReader.readValue(order, streamReader);
+                    values.add(value);
+                }
+            }
+            return StringUtils.join(values, ",");
         }
     };
 
